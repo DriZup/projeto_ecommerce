1eaa03f19784fb25d6b9362713727b81187659dd
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-28 17:19:40 -0300

    feat: criando tabelas e relacionamentos de clientes

---
+++ src/main/java/adriana/nogueira/e_commerce/model/Cliente.java
@@ -0,0 +1,63 @@
+package adriana.nogueira.e_commerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank(message = "O nome é obrigatório.")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres.")
+    private String nome;
+
+    @NotBlank(message = "O CPF é obrigatório.")
+    @Size(min = 11, max = 11, message = "O CPF deve ter 11 caracteres.")
+    @Column(unique = true)
+    private String cpf;
+
+    @NotBlank(message = "O e-mail é obrigatório.")
+    @Email(message = "O e-mail deve ser válido.")
+    @Column(unique = true)
+    private String email;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
