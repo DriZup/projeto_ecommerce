f45a7972470763a510aa6b4ed8052d43fc48343d
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-30 10:25:42 -0300

    refactor: melhorar organização e padronização das requisições no ClienteController - Refatorado os endpoints para melhor legibilidade e consistência. - Ajustado o mapeamento de URLs para seguir boas práticas REST. - Melhorada a estrutura dos métodos para maior clareza e manutenção. - Garantido o uso de boas práticas no tratamento de respostas HTTP.

--- src/main/java/adriana/nogueira/e_commerce/controller/ClienteController.java
+++ src/main/java/adriana/nogueira/e_commerce/controller/ClienteController.java
@@ -1,31 +1,47 @@
 package adriana.nogueira.e_commerce.controller;
 
 import adriana.nogueira.e_commerce.model.Cliente;
-import adriana.nogueira.e_commerce.service.ClienteService;
+import adriana.nogueira.e_commerce.service.ClienteServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
-    private ClienteService clienteService;
+    private ClienteServiceImpl clienteService;
 
     @PostMapping("/salvar")
-    public ResponseEntity<?> cadastrarCliente(@Valid @RequestBody Cliente cliente) {
-        try {
-            Cliente clienteSalvo = clienteService.salvarCliente(cliente);
-            return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<Cliente> cadastrarCliente(@Valid @RequestBody Cliente cliente) {
+       Cliente salvarCliente = clienteService.salvarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(salvarCliente);
+    }
 
-        }
+    @PutMapping("/atualizar")
+    public ResponseEntity<Cliente> atualizarCliente(@Valid @RequestBody Cliente cliente) {
+        clienteService.atualizarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.OK).body(cliente);
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        Cliente buscarPorId = clienteService.buscarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(buscarPorId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+        Iterable<Cliente> buscarTodos = clienteService.buscarTodos();
+        return ResponseEntity.status(HttpStatus.OK).body(buscarTodos);
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity<Void> excluirCliente(@PathVariable Long id) {
+        clienteService.excluirCliente(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
