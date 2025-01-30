b1ecc319bfa03d7c8f53bc8ed3278d7eac7401a4
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-30 10:27:28 -0300

    feat: adicionar método findByNomeContainingIgnoreCase no ProdutoRepository - Criado método para buscar produtos por nome, ignorando maiúsculas e minúsculas e permitindo busca parcial. - Facilita a implementação de buscas mais flexíveis no sistema.

--- src/main/java/adriana/nogueira/e_commerce/repository/ProdutoRepository.java
+++ src/main/java/adriana/nogueira/e_commerce/repository/ProdutoRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     boolean existsByNome(String nome);
+
+    Iterable<Produto> findByNomeContainingIgnoreCase(String nome);
 }
