1e1d6b6739f27131d0d8f4334cbc6c6536672ecf
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-30 10:28:07 -0300

    criação de um crud para produtos.

---
+++ src/main/java/adriana/nogueira/e_commerce/controller/ProdutoController.java
@@ -0,0 +1,47 @@
+package adriana.nogueira.e_commerce.controller;
+
+import adriana.nogueira.e_commerce.model.Produto;
+import adriana.nogueira.e_commerce.service.ProdutoServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoServiceImpl produtoService;
+
+    @PostMapping("/salvar")
+    public ResponseEntity<Produto> salvarProduto(@Valid @RequestBody Produto produto) {
+        Produto salvarProduto = produtoService.salvarProduto(produto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(salvarProduto);
+    }
+
+    @PutMapping("/atualizar")
+    public ResponseEntity<Produto> atualizarProduto( @Valid @RequestBody Produto produto) {
+        Produto atualizarProduto = produtoService.atualizarProduto(produto);
+        return ResponseEntity.status(HttpStatus.OK).body(atualizarProduto);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        produtoService.deletarProduto(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Produto>> listarProdutos() {
+        Iterable<Produto> listarProdutos = produtoService.listarProdutos();
+        return ResponseEntity.status(HttpStatus.OK).body(listarProdutos);
+    }
+
+    @GetMapping("/buscar/{nome}")
+    public ResponseEntity<Iterable<Produto>> buscarProdutosPorNome(@PathVariable String nome) {
+        Iterable<Produto> buscarProdutosPorNome = produtoService.buscarProdutosPorNome(nome);
+        return ResponseEntity.status(HttpStatus.OK).body(buscarProdutosPorNome);
+    }
+}
