0eed00b4f7d62f29b402442d7fd411edc1d17455
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-02-02 15:03:23 -0300

    feat: criação  da entitidade compra

---
+++ src/main/java/adriana/nogueira/e_commerce/model/Compra.java
@@ -0,0 +1,48 @@
+package adriana.nogueira.e_commerce.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToMany
+    @JoinTable(
+            name = "compra_produto",
+            joinColumns = @JoinColumn(name = "compra_id"),
+            inverseJoinColumns = @JoinColumn(name = "produto_id")
+    )
+    private List<Produto> produtos;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
