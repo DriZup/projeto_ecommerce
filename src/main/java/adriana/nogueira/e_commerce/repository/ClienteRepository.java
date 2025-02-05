7f393b68f6e44ba809c45e4859eba4c82a2c679b
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-02-05 13:58:34 -0300

    refactor: autalizando a repository cliente

--- src/main/java/adriana/nogueira/e_commerce/repository/ClienteRepository.java
+++ src/main/java/adriana/nogueira/e_commerce/repository/ClienteRepository.java
@@ -1,17 +1,17 @@
 package adriana.nogueira.e_commerce.repository;
 
 import adriana.nogueira.e_commerce.model.Cliente;
+import adriana.nogueira.e_commerce.service.ClienteService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+
+   Cliente findByCpf(String cpf);
+
     boolean existsByCpf(String cpf);
 
     boolean existsByEmail(String email);
-
-    Optional<Cliente> findByCpf(String cpf);
-
-    Cliente findByEmail(String email);
 }
