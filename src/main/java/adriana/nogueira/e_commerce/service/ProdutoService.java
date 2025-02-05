c2601e861d2073ea2e4dccd19527dbf2a448bc6d
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-02-05 14:01:46 -0300

    refactor: alterando a interface do service de compra

--- src/main/java/adriana/nogueira/e_commerce/service/ProdutoService.java
+++ src/main/java/adriana/nogueira/e_commerce/service/ProdutoService.java
@@ -2,18 +2,12 @@ package adriana.nogueira.e_commerce.service;
 
 import adriana.nogueira.e_commerce.model.Produto;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ProdutoService {
 
-    public Produto salvarProduto(Produto produto);
 
-    public Produto atualizarProduto(Produto produto);
-
-    public Map<String, String> deletarProduto(Long id);
-
-    public Iterable<Produto> listarProdutos();
-
-    public Iterable<Produto> buscarProdutosPorNome(String nome);
-
+    public List<Produto> listarProdutos();
+    Produto salvar(Produto produto);
 }
