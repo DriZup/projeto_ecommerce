7da60769ec10264f79b6a1e5854f8473bda0d1c8
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-02-05 14:02:38 -0300

    refactor: refatoração da produto controller para melhora e performace

--- src/main/java/adriana/nogueira/e_commerce/controller/ProdutoController.java
+++ src/main/java/adriana/nogueira/e_commerce/controller/ProdutoController.java
@@ -1,49 +1,34 @@
 package adriana.nogueira.e_commerce.controller;
 
+import adriana.nogueira.e_commerce.dto.ProdutoDTO;
 import adriana.nogueira.e_commerce.model.Produto;
-import adriana.nogueira.e_commerce.service.ProdutoServiceImpl;
+import adriana.nogueira.e_commerce.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
     @Autowired
-    private ProdutoServiceImpl produtoService;
+    private ProdutoService produtoService;
 
-    @PostMapping("/salvar")
-    public ResponseEntity<Produto> salvarProduto(@RequestBody Produto produto) {
-        Produto salvarProduto = produtoService.salvarProduto(produto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(salvarProduto);
-    }
-
-    @PutMapping("/atualizar")
-    public ResponseEntity<Produto> atualizarProduto(@RequestBody Produto produto) {
-        Produto atualizarProduto = produtoService.atualizarProduto(produto);
-        return ResponseEntity.status(HttpStatus.OK).body(atualizarProduto);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, String>> deletarProduto(@PathVariable Long id) {
-        Map<String, String> response = produtoService.deletarProduto(id);
-        return ResponseEntity.ok(response);
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> salvarProduto(@RequestBody ProdutoDTO produtoDTO) {
+        Produto produto = produtoDTO.toModel();
+        Produto produtoSalvo = produtoService.salvar(produto);
+        ProdutoDTO response = ProdutoDTO.fromModel(produtoSalvo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public ResponseEntity<Iterable<Produto>> listarProdutos() {
-        Iterable<Produto> listarProdutos = produtoService.listarProdutos();
-        return ResponseEntity.status(HttpStatus.OK).body(listarProdutos);
-    }
-
-    @GetMapping("/buscar/{nome}")
-    public ResponseEntity<Iterable<Produto>> buscarProdutosPorNome(@PathVariable String nome) {
-        Iterable<Produto> buscarProdutosPorNome = produtoService.buscarProdutosPorNome(nome);
-        return ResponseEntity.status(HttpStatus.OK).body(buscarProdutosPorNome);
+    public ResponseEntity<List<Produto>> listarProdutos() {
+        List<Produto> produtos = produtoService.listarProdutos();
+        return ResponseEntity.ok(produtos);
     }
 }
